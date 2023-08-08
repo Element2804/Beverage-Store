533a385bff3c812450316221e56f367b1e8ae22b
Jose <josejrod07@gmail.com>
2023-08-07 21:11:34 -0400

    deploy 3

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,8 +1,4 @@
-require('dotenv').config();
-
 const mongoose = require('mongoose');
-
-console.log(require('dotenv').config());
 
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/gulp-co');
 
